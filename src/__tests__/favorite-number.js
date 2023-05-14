a55c93d62acf2575c2a35471b4bf39730af88271
Samuel Gomez <contact@samuelgomez.fr>
2023-05-14 17:01:29 +0200

    feat(render) : solution

---
+++ src/__tests__/favorite-number.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import {render, screen} from '@testing-library/react'
+import {FavoriteNumber} from '../favorite-number'
+
+test('Render a number input with a label Favorite Number', () => {
+  render(<FavoriteNumber />)
+  const input = screen.getByLabelText(/favorite number/i)
+  expect(input).toHaveAttribute('type', 'number')
+})
