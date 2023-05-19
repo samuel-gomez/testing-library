166c4b6b9216b668ec9f00948b93288ba12175d8
Samuel Gomez <contact@samuelgomez.fr>
2023-05-19 18:56:38 +0200

    feat(mockapi) : solution3

--- src/__tests__/greeting-loader.js
+++ src/__tests__/greeting-loader.js
@@ -1,15 +1,33 @@
 import React from 'react'
+import 'whatwg-fetch'
 import {render, screen, waitFor} from '@testing-library/react'
+import {setupServer} from 'msw/node'
+import {rest} from 'msw'
 import user from '@testing-library/user-event'
 import {GreetingLoader} from '../greeting-loader'
 
-test('Loads greetings on click', async () => {
-  const TEST_GREETING = 'test greeting'
-  const mockLoadGreeting = jest
-    .fn()
-    .mockReturnValueOnce({data: {greeting: TEST_GREETING}})
+// on créé notre serveur
+// on définit le endpoint que l'on cherche à intercepter
+// la fonction de callback reçoit la requête avec les données envoyées
+// on retourne une réponse au format json en étant fidèle à l'API
+const server = setupServer(
+  rest.post('/greeting', (req, res, ctx) => {
+    return res(ctx.json({data: {greeting: `Hello ${req.body.subject}`}}))
+  }),
+)
 
-  render(<GreetingLoader loadGreeting={mockLoadGreeting} />)
+// Avant les tests, on déclenche le serveur d'écoute qui interceptera toutes les requêtes HTTP
+// en cas d'erreurs de frappe ou d'appels non souhaités, on affiche une erreur avec onUnhandledRequest
+beforeAll(() => server.listen({onUnhandledRequest: 'error'}))
+
+// on éteint le serveur après les tests
+afterAll(() => server.close())
+
+// Après chaque test, si d'autres handlers sont ajoutés, ils seront réinitialisés
+afterEach(() => server.resetHandlers())
+
+test('Loads greetings on click', async () => {
+  render(<GreetingLoader />)
 
   // pour récupérer un élément via le aria-label ou le label d'un champ on utilise getByLabelText
   const nameInput = screen.getByLabelText(/name/i)
@@ -20,11 +38,7 @@ test('Loads greetings on click', async () => {
 
   user.click(loadButton)
 
-  // on vérifie si notre API a été appelé 1 fois et avec les bons arguments
-  expect(mockLoadGreeting).toHaveBeenCalledWith(nameInput.value)
-  expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
-
   await waitFor(() => {
-    expect(screen.getByLabelText(/greeting/i)).toHaveTextContent(TEST_GREETING)
+    expect(screen.getByLabelText(/greeting/i)).toHaveTextContent('Hello Mary')
   })
 })
