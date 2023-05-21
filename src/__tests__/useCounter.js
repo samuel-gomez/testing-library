8d7ab247a0e44f71b51f8db84d3faa9ce41929f1
Samuel Gomez <contact@samuelgomez.fr>
2023-05-21 11:05:52 +0200

    feat(customhook) : solution2

--- src/__tests__/useCounter.js
+++ src/__tests__/useCounter.js
@@ -2,16 +2,35 @@ import * as React from 'react'
 import {render, act} from '@testing-library/react'
 import {useCounter} from '../useCounter'
 
-test('exposes the count and increment/decrement functions', () => {
-  let result
-  function TestComponent() {
-    result = useCounter()
+const setup = ({initialProps} = {}) => {
+  const result = {}
+  function TestComponent(props) {
+    result.current = useCounter(props)
     return null
   }
-  render(<TestComponent />)
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  render(<TestComponent {...initialProps} />)
+  return result
+}
+
+test('exposes the count and increment/decrement functions', () => {
+  const result = setup()
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
+
+test('allows customization of the initial count', () => {
+  const result = setup({initialProps: {initialCount: 3}})
+  expect(result.current.count).toBe(3)
+})
+
+test('allows customization of the step', () => {
+  const result = setup({initialProps: {step: 2}})
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
